f67aedf265cd45115a8d686494007726426a0a63
Dmitrij-Shagojko <dmitry2844@gmail.com>
2022-07-31 21:18:32 +0300

    Added method getBookByIsbn to class BookDAOImpl.

--- src/BookDAOImpl.java
+++ src/BookDAOImpl.java
@@ -1,12 +1,11 @@
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BookDAOImpl implements BookDAO{
     public static final String GET_ALL = "SELECT id, name, author, publishment_date FROM books WHERE delete_book IS NULL";
+    public static final String GET_BOOK_BY_ISBN10 = "SELECT id, name, author, publishment_date FROM books WHERE \"ISBN-10\" = ?";
+    public static final String GET_BOOK_BY_ISBN13 = "SELECT id, name, author, publishment_date FROM books WHERE \"ISBN-13\" = ?";
 
     private final DataSource dataSource;
 
@@ -33,9 +32,30 @@ public class BookDAOImpl implements BookDAO{
             e.printStackTrace();
         } return books;
     }
-
+    
     @Override
     public Book getBookByIsbn(String isbn) {
+        try {
+            Connection connection = dataSource.getConnection();
+            PreparedStatement statement = null;
+            if (isbn.length() == 10){
+                statement = connection.prepareStatement(GET_BOOK_BY_ISBN10);
+            } else {
+                statement = connection.prepareStatement(GET_BOOK_BY_ISBN13);
+            }
+            statement.setString(1, isbn);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()){
+                Book book = new Book();
+                book.setId(resultSet.getLong("id"));
+                book.setName(resultSet.getString("name"));
+                book.setAuthor(resultSet.getString("author"));
+                book.setPublishmentDate(resultSet.getString("publishment_date"));
+                return book;
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
         return null;
     }
 
