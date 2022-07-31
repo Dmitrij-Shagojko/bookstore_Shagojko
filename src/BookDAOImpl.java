cde5a2308603447f8da002195501f104e1d62c2d
Dmitrij-Shagojko <dmitry2844@gmail.com>
2022-07-31 20:48:39 +0300

    Added method getAll to class BookDAOImpl.

--- src/BookDAOImpl.java
+++ src/BookDAOImpl.java
@@ -1,9 +1,37 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BookDAOImpl implements BookDAO{
+    public static final String GET_ALL = "SELECT id, name, author, publishment_date FROM books WHERE delete_book IS NULL";
+
+    private final DataSource dataSource;
+
+    public BookDAOImpl(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     public List<Book> getAll() {
-        return null;
+        List<Book> books = new ArrayList<>();
+        try {
+            Connection connection = dataSource.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(GET_ALL);
+            while (resultSet.next()){
+            Book book = new Book();
+            book.setId(resultSet.getLong("id"));
+            book.setName(resultSet.getString("name"));
+            book.setAuthor(resultSet.getString("author"));
+            book.setPublishmentDate(resultSet.getString("publishment_date"));
+            books.add(book);
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        } return books;
     }
 
     @Override
