4f83378fd8ce585bd7aed5f3e39eaab22f61d0b2
Dmitrij-Shagojko <dmitry2844@gmail.com>
2022-08-07 00:44:53 +0300

    Added method getAll to class UserDAOImpl

---
+++ src/UserDAOImpl.java
@@ -0,0 +1,81 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAOImpl implements UserDAO{
+    public static final String GET_ALL_USER = """
+            SELECT u.id, u.first_name, u.last_name, u.email, r.role AS role
+            FROM users u
+            JOIN roles r
+            ON u.role_id = r.id
+            ORDER BY u.id""";
+
+    private DataSource dataSource;
+
+    public UserDAOImpl(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+    private User setUser(ResultSet resultSet) throws SQLException {
+        User user = new User();
+        user.setId(resultSet.getLong("id"));
+        user.setFirstName(resultSet.getString("first_name"));
+        user.setLastName(resultSet.getString("last_name"));
+        user.setEmail(resultSet.getString("email"));
+        user.setRole(User.Role.valueOf(resultSet.getString("role")));
+        return user;
+    }
+
+    @Override
+    public List<User> getAll() {
+        Connection connection = dataSource.getConnection();
+        List<User> users = new ArrayList<>();
+        try(Statement statement = connection.createStatement()){
+            ResultSet resultSet = statement.executeQuery(GET_ALL_USER);
+            while (resultSet.next()){
+                User user = setUser(resultSet);
+                users.add(user);
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return null;
+    }
+
+    @Override
+    public User create(User user) {
+        return null;
+    }
+
+    @Override
+    public User update(User user) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+
+    @Override
+    public User getUserByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public List<User> getUserByLastName(String lastName) {
+        return null;
+    }
+
+    @Override
+    public int countAllUsers() {
+        return 0;
+    }
+}
