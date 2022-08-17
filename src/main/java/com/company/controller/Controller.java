4905253cd441f2113b27b606705d5500e0cd6d86
Dmitrij-Shagojko <dmitry2844@gmail.com>
2022-08-17 11:15:52 +0300

    Created new method in Controller to processing requests

--- src/main/java/com/company/controller/Controller.java
+++ src/main/java/com/company/controller/Controller.java
@@ -14,28 +14,30 @@ public class Controller extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String commandParam = req.getParameter("command");
-        Command command = CommandFactory.INSTANCE.getCommand(commandParam);
-        String page;
-        try{
-            page = command.execute(req);
-        } catch (NullPointerException e){
-            int status = 400;
-            req.setAttribute("errorStatus", status);
-            String message = "Wrong command entered";
-            req.setAttribute("message", message);
-            page =  "error.jsp";
-        }
-        req.getRequestDispatcher(page).forward(req, resp);
+        processing(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processing(req, resp);
+    }
+
+    private void processing(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String commandParam = req.getParameter("command");
         Command command = CommandFactory.INSTANCE.getCommand(commandParam);
-        String page = command.execute(req);
+        String page;
+        try {
+            page = command.execute(req);
+        } catch (NullPointerException e) {
+            int status = 400;
+            req.setAttribute("errorStatus", status);
+            String message = "Wrong command entered";
+            req.setAttribute("message", message);
+            page = "error.jsp";
+        }
         req.getRequestDispatcher(page).forward(req, resp);
     }
+
 
     @Override
     public void destroy() {
